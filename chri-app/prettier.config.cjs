ffd2abc0e8c5401ba36e084ad4eafa9284ac4d47
Rekcah-Chris <muasyamwendwa855@gmail.com>
2024-11-13 16:43:32 +0300

    packages

---
+++ chri-app/prettier.config.cjs
@@ -0,0 +1,10 @@
+// module.export ={plugins: [require('prettier-plugin-tailwindcss')], sinlgeQuote:mtrue,}
+
+// .prettierrc
+
+{
+  ("plugins");
+  ["prettier-plugin-tailwindcss"];
+  ("singleQuote");
+  true;
+}
